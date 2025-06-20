1364cb456041bc6df2557ae73a53b0972dae6002
Boris <glazunov2142@icloud.com>
2025-06-20 10:59:39 +0300

    Update config_v2.go

--- pkg/config/config_v2.go
+++ pkg/config/config_v2.go
@@ -13,15 +13,15 @@ type V2 struct {
 }
 
 type CacheV2 struct {
-	Enabled  bool          `yaml:"enabled"`
-	Eviction EvictionV2    `yaml:"eviction"`
-	Refresh  RefreshV2     `yaml:"refresh"`
-	Storage  StorageV2     `yaml:"storage"`
-	Rules    []CacheRuleV2 `yaml:"rules"`
+	Enabled  bool           `yaml:"enabled"`
+	Eviction EvictionV2     `yaml:"eviction"`
+	Refresh  RefreshV2      `yaml:"refresh"`
+	Storage  StorageV2      `yaml:"storage"`
+	Rules    []*CacheRuleV2 `yaml:"rules"`
 }
 
 type EvictionV2 struct {
-	Policy    string  `yaml:"policy"`    // at now, it's only "lru" with works per shard
+	Policy    string  `yaml:"policy"`    // at now, it's only "lru" with works
 	Threshold float64 `yaml:"threshold"` // 0.9 means 90%
 }
 
@@ -41,53 +41,76 @@ type RefreshV2 struct {
 	// expireTime = ttl * (-beta * ln(random()))
 	// Подробнее: RFC 5861 и https://web.archive.org/web/20100829170210/http://labs.google.com/papers/staleness.pdf
 	// beta: "0.4"
-	Beta     float64       `yaml:"beta"`      // between 0 and 1
-	MinStale time.Duration `yaml:"min_stale"` // computed=time.Duration(float64(TTL/ErrorTTL) * Beta)
+	Beta       float64       `yaml:"beta"`      // between 0 and 1
+	MinStale   time.Duration `yaml:"min_stale"` // computed=time.Duration(float64(TTL/ErrorTTL) * Beta)
+	BackendURL string        `yaml:"backend_url"`
 }
 
 type CacheRuleV2 struct {
-	Path       string     `yaml:"path"`
+	Path       string `yaml:"path"`
+	PathBytes  []byte
 	CacheKey   CacheKey   `yaml:"cache_key"`
 	CacheValue CacheValue `yaml:"cache_value"`
 }
 
 type CacheKey struct {
-	Query   []string `yaml:"query"`
-	Headers []string `yaml:"headers"`
+	Query        []string `yaml:"query"` // Параметры, которые будут участвовать в ключе кэширования
+	QueryBytes   [][]byte
+	Headers      []string `yaml:"headers"` // Хедеры, которые будут участвовать в ключе кэширования
+	HeadersBytes [][]byte
 }
 
 type CacheValue struct {
-	Headers []string `yaml:"headers"`
+	Headers      []string `yaml:"headers"` // Хедеры ответа, которые будут сохранены в кэше вместе с body
+	HeadersBytes [][]byte
 }
 
 const (
-	configPath      = "../../config/config.yaml"
-	configPathLocal = "../../config/config.local.yaml"
+	configPath      = "/config/config.yaml"
+	configPathLocal = "/config/config.local.yaml"
 )
 
-func LoadConfigV2() (V2, error) {
+func LoadConfigV2() (*V2, error) {
 	var path string
 
-	if _, err := os.Stat(configPathLocal); err == nil {
-		path = configPathLocal
+	dir, err := os.Getwd()
+	if err != nil {
+		return nil, err
+	}
+
+	if _, err = os.Stat(dir + configPathLocal); err == nil {
+		path = dir + configPathLocal
 	} else if os.IsNotExist(err) {
-		if _, err = os.Stat(configPath); err == nil {
-			path = configPath
+		if _, err = os.Stat(dir + configPath); err == nil {
+			path = dir + configPath
 		} else {
-			return V2{}, errors.New("config does not exist by path: " + configPath + " or " + configPathLocal)
+			return nil, errors.New("config does not exist by path: " + dir + configPath + " or " + dir + configPathLocal)
 		}
 	} else {
-		return V2{}, fmt.Errorf("stat config path: %w", err)
+		return nil, fmt.Errorf("stat config path: %w", err)
 	}
 
 	data, err := os.ReadFile(path)
 	if err != nil {
-		return V2{}, fmt.Errorf("read config yaml file %s: %w", path, err)
+		return nil, fmt.Errorf("read config yaml file %s: %w", path, err)
 	}
 
-	var cfg V2
+	var cfg *V2
 	if err = yaml.Unmarshal(data, &cfg); err != nil {
-		return V2{}, fmt.Errorf("unmarshal yaml from %s: %w", path, err)
+		return nil, fmt.Errorf("unmarshal yaml from %s: %w", path, err)
+	}
+
+	for k, rule := range cfg.Cache.Rules {
+		cfg.Cache.Rules[k].PathBytes = []byte(rule.Path)
+		for _, param := range rule.CacheKey.Query {
+			cfg.Cache.Rules[k].CacheKey.QueryBytes = append(cfg.Cache.Rules[k].CacheKey.QueryBytes, []byte(param))
+		}
+		for _, param := range rule.CacheKey.Headers {
+			cfg.Cache.Rules[k].CacheKey.HeadersBytes = append(cfg.Cache.Rules[k].CacheKey.HeadersBytes, []byte(param))
+		}
+		for _, param := range rule.CacheValue.Headers {
+			cfg.Cache.Rules[k].CacheValue.HeadersBytes = append(cfg.Cache.Rules[k].CacheValue.HeadersBytes, []byte(param))
+		}
 	}
 
 	cfg.Cache.Refresh.MinStale = time.Duration(float64(cfg.Cache.Refresh.TTL) * cfg.Cache.Refresh.Beta)
