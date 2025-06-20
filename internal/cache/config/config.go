e5a659b64c13c9acee554a55ea050437d143f6ba
Boris <glazunov2142@icloud.com>
2025-06-20 12:17:55 +0300

    Update config.go

--- internal/cache/config/config.go
+++ internal/cache/config/config.go
@@ -2,13 +2,14 @@ package config
 
 import (
 	cacheConfig "github.com/Borislavv/traefik-http-cache-plugin/pkg/config"
+	"github.com/Borislavv/traefik-http-cache-plugin/pkg/k8s/probe/liveness"
 	prometheusconifg "github.com/Borislavv/traefik-http-cache-plugin/pkg/prometheus/metrics/config"
 	fasthttpconfig "github.com/Borislavv/traefik-http-cache-plugin/pkg/server/config"
 )
 
 type Config struct {
-	*cacheConfig.V2          // loads from yaml
-	cacheConfig.Cache        `mapstructure:",squash"`
+	*cacheConfig.Cache       // loads from yaml
 	prometheusconifg.Metrics `mapstructure:",squash"`
 	fasthttpconfig.Server    `mapstructure:",squash"`
+	liveness.Config          `mapstructure:",squash"`
 }
