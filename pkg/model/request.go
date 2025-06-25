a9b8097cbfc314033ba60fb83929a13b660784c2
Boris <glazunov2142@icloud.com>
2025-06-25 15:47:33 +0300

    Update request.go

--- pkg/model/request.go
+++ pkg/model/request.go
@@ -6,6 +6,7 @@ import (
 	sharded "github.com/Borislavv/advanced-cache/pkg/storage/map"
 	"github.com/valyala/fasthttp"
 	"github.com/zeebo/xxh3"
+	"net/http"
 	"sort"
 	"sync"
 	"unsafe"
@@ -19,6 +20,114 @@ type Request struct {
 	shard uint64
 	query []byte
 	path  []byte
+}
+
+func NewRequestFromNetHttp(cfg *config.Cache, r *http.Request) *Request {
+	path := append([]byte(nil), r.URL.Path...)
+
+	req := &Request{path: path}
+
+	rule := matchRule(cfg, path)
+	if rule != nil {
+		req.rule = rule
+		sanitizeNetHttpRequest(rule, r)
+	}
+
+	setUpNetHttp(req, path, r)
+
+	return req
+}
+
+func sanitizeNetHttpRequest(rule *config.Rule, r *http.Request) {
+	queries := r.URL.Query()
+	for key := range queries {
+		if !keyAllowed(rule.CacheKey.QueryBytes, key) {
+			queries.Del(key)
+		}
+	}
+	r.URL.RawQuery = queries.Encode()
+
+	for key := range r.Header {
+		if !keyAllowed(rule.CacheKey.HeadersBytes, key) {
+			r.Header.Del(key)
+		}
+	}
+}
+
+func keyAllowed(allowed [][]byte, key string) bool {
+	keyBytes := []byte(key)
+	for _, allowedKey := range allowed {
+		if bytes.EqualFold(keyBytes, allowedKey) {
+			return true
+		}
+	}
+	return false
+}
+
+func setUpNetHttp(r *Request, path []byte, req *http.Request) {
+	queries := req.URL.Query()
+
+	var sortedArgs []struct{ Key, Value string }
+	argsLength := 1
+	for key, vals := range queries {
+		for _, val := range vals {
+			sortedArgs = append(sortedArgs, struct{ Key, Value string }{key, val})
+			argsLength += len(key) + len(val) + 2
+		}
+	}
+
+	sort.Slice(sortedArgs, func(i, j int) bool {
+		return sortedArgs[i].Key < sortedArgs[j].Key
+	})
+
+	argsBuf := make([]byte, 0, argsLength)
+	if len(sortedArgs) > 0 {
+		argsBuf = append(argsBuf, '?')
+		for _, arg := range sortedArgs {
+			argsBuf = append(argsBuf, arg.Key...)
+			argsBuf = append(argsBuf, '=')
+			argsBuf = append(argsBuf, arg.Value...)
+			argsBuf = append(argsBuf, '&')
+		}
+		argsBuf = argsBuf[:len(argsBuf)-1] // remove trailing '&'
+	}
+
+	sortedHeaders := make([]struct{ Key, Value string }, 0, len(req.Header))
+	headersLength := 0
+	for key, vals := range req.Header {
+		for _, val := range vals {
+			sortedHeaders = append(sortedHeaders, struct{ Key, Value string }{key, val})
+			headersLength += len(key) + len(val) + 2
+		}
+	}
+
+	sort.Slice(sortedHeaders, func(i, j int) bool {
+		return sortedHeaders[i].Key < sortedHeaders[j].Key
+	})
+
+	headersBuf := make([]byte, 0, headersLength)
+	for _, hdr := range sortedHeaders {
+		headersBuf = append(headersBuf, hdr.Key...)
+		headersBuf = append(headersBuf, ':')
+		headersBuf = append(headersBuf, hdr.Value...)
+		headersBuf = append(headersBuf, '\n')
+	}
+	if len(headersBuf) > 0 {
+		headersBuf = headersBuf[:len(headersBuf)-1] // remove trailing '\n'
+	}
+
+	bufLen := len(path) + len(argsBuf) + len(headersBuf) + 1
+	buf := make([]byte, 0, bufLen)
+	if bufLen > 1 {
+		buf = append(buf, path...)
+		buf = append(buf, argsBuf...)
+		buf = append(buf, '\n')
+		buf = append(buf, headersBuf...)
+	}
+
+	r.query = argsBuf
+	r.key = hash(buf)
+	r.shard = sharded.MapShardKey(r.key)
 }
 
 func NewRequestFromFasthttp(cfg *config.Cache, r *fasthttp.RequestCtx) *Request {
