cb024c99e7e01fc4f5de15216f51813a91ea4f4e
Boris <glazunov2142@icloud.com>
2025-07-05 12:12:28 +0300

    Update backend.go

--- pkg/repository/backend.go
+++ pkg/repository/backend.go
@@ -7,8 +7,36 @@ import (
 	"github.com/Borislavv/advanced-cache/pkg/config"
 	"github.com/Borislavv/advanced-cache/pkg/model"
 	"io"
+	"net"
 	"net/http"
+	"sync"
+	"time"
 )
+
+var transport = &http.Transport{
+	// Max idle (keep-alive) connections across ALL hosts
+	MaxIdleConns: 10000,
+
+	// Max idle (keep-alive) connections per host
+	MaxIdleConnsPerHost: 1000,
+
+	// Max concurrent connections per host (optional)
+	MaxConnsPerHost: 0, // 0 = unlimited (use with caution)
+
+	IdleConnTimeout: 30 * time.Second,
+
+	// Optional: tune dialer
+	DialContext: (&net.Dialer{
+		Timeout:   30 * time.Second,
+		KeepAlive: 30 * time.Second,
+	}).DialContext,
+
+	// Optional: configure TLS handshake timeout, etc.
+	TLSHandshakeTimeout: 10 * time.Second,
+
+	// ExpectContinueTimeout: wait time for 100-continue
+	ExpectContinueTimeout: 1 * time.Second,
+}
 
 // Backender defines the interface for a repository that provides SEO page data.
 type Backender interface {
@@ -19,12 +47,21 @@ type Backender interface {
 // Backend implements the Backender interface.
 // It fetches and constructs SEO page data responses from an external backend.
 type Backend struct {
-	cfg *config.Cache // Global configuration (backend URL, etc)
+	cfg         *config.Cache // Global configuration (backend URL, etc)
+	transport   *http.Transport
+	clientsPool *sync.Pool
 }
 
 // NewBackend creates a new instance of Backend.
 func NewBackend(cfg *config.Cache) *Backend {
-	return &Backend{cfg: cfg}
+	return &Backend{cfg: cfg, clientsPool: &sync.Pool{
+		New: func() interface{} {
+			return &http.Client{
+				Transport: transport,
+				Timeout:   10 * time.Second,
+			}
+		},
+	}}
 }
 
 // Fetch method fetches page data for the given request and constructs a cacheable response.
@@ -77,7 +114,10 @@ func (s *Backend) requestExternalBackend(ctx context.Context, req *model.Request
 		request.Header.Add(string(hdr[0]), string(hdr[1]))
 	}
 
-	response, err := http.DefaultClient.Do(request)
+	client := s.clientsPool.Get().(*http.Client)
+	defer s.clientsPool.Put(client)
+
+	response, err := client.Do(request)
 	if err != nil {
 		return nil, err
 	}
