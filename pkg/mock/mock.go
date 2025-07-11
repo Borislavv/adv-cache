44efcfb09908c0da1943039b4d5f49edf4c2f4df
Boris <glazunov2142@icloud.com>
2025-07-11 12:25:14 +0300

    Update mock.go

--- pkg/mock/mock.go
+++ pkg/mock/mock.go
@@ -6,99 +6,9 @@ import (
 	"github.com/Borislavv/advanced-cache/pkg/config"
 	"github.com/Borislavv/advanced-cache/pkg/model"
 	"github.com/Borislavv/advanced-cache/pkg/repository"
-	"net/http"
 	"runtime"
 	"strconv"
 )
-
-// GenerateRandomRequests produces a slice of *model.Request for use in tests and benchmarks.
-// Each request gets a unique combination of project, domain, language, and tags.
-func GenerateRandomRequests(cfg *config.Cache, path []byte, num int) []*model.Request {
-	i := 0
-	list := make([]*model.Request, 0, num)
-
-	// Iterate over all possible language and project ID combinations until num requests are created
-	for {
-		//for _, lng := range localesandlanguages.LanguageCodeList() {
-		//for projectID := 1; projectID < 1000; projectID++ {
-		if i >= num {
-			return list
-		}
-		req := model.NewRequest(
-			cfg,
-			path,
-			[][2][]byte{
-				{[]byte("project[id]"), []byte("285")},
-				{[]byte("domain"), []byte("1x001.com")},
-				{[]byte("language"), []byte("en")},
-				{[]byte("choice[name]"), []byte("betting")},
-				{[]byte("choice[choice][name]"), []byte("betting_live")},
-				{[]byte("choice[choice][choice][name]"), []byte("betting_live_null")},
-				{[]byte("choice[choice][choice][choice][name]"), []byte("betting_live_null_" + strconv.Itoa(i))},
-				{[]byte("choice[choice][choice][choice][choice][name]"), []byte("betting_live_null_" + strconv.Itoa(i) + "_" + strconv.Itoa(i))},
-				{[]byte("choice[choice][choice][choice][choice][choice][name]"), []byte("betting_live_null_" + strconv.Itoa(i) + "_" + strconv.Itoa(i) + "_" + strconv.Itoa(i))},
-				{[]byte("choice[choice][choice][choice][choice][choice][choice]"), []byte("null")},
-			},
-			[][2][]byte{
-				{[]byte("Host"), []byte("0.0.0.0:8020")},
-				{[]byte("Accept-Encoding"), []byte("gzip, deflate, br")},
-				{[]byte("Accept-Language"), []byte("en-US,en;q=0.9")},
-				{[]byte("Content-Type"), []byte("application/json")},
-			},
-		)
-		list = append(list, req)
-		i++
-		//}
-		//}
-	}
-}
-
-func StreamRandomRequests(ctx context.Context, cfg *config.Cache, path []byte, num int) <-chan *model.Request {
-	i := 0
-	out := make(chan *model.Request)
-
-	go func() {
-		defer close(out)
-
-		// Iterate over all possible language and project ID combinations until num requests are created
-		for {
-			select {
-			case <-ctx.Done():
-				return
-			default:
-				if i >= num {
-					return
-				}
-				req := model.NewRequest(
-					cfg,
-					path,
-					[][2][]byte{
-						{[]byte("project[id]"), []byte("285")},
-						{[]byte("domain"), []byte("1x001.com")},
-						{[]byte("language"), []byte("en")},
-						{[]byte("choice[name]"), []byte("betting")},
-						{[]byte("choice[choice][name]"), []byte("betting_live")},
-						{[]byte("choice[choice][choice][name]"), []byte("betting_live_null")},
-						{[]byte("choice[choice][choice][choice][name]"), []byte("betting_live_null_" + strconv.Itoa(i))},
-						{[]byte("choice[choice][choice][choice][choice][name]"), []byte("betting_live_null_" + strconv.Itoa(i) + "_" + strconv.Itoa(i))},
-						{[]byte("choice[choice][choice][choice][choice][choice][name]"), []byte("betting_live_null_" + strconv.Itoa(i) + "_" + strconv.Itoa(i) + "_" + strconv.Itoa(i))},
-						{[]byte("choice[choice][choice][choice][choice][choice][choice]"), []byte("null")},
-					},
-					[][2][]byte{
-						{[]byte("Host"), []byte("0.0.0.0:8020")},
-						{[]byte("Accept-Encoding"), []byte("gzip, deflate, br")},
-						{[]byte("Accept-Language"), []byte("en-US,en;q=0.9")},
-						{[]byte("Content-Type"), []byte("application/json")},
-					},
-				)
-				out <- req
-				i++
-			}
-		}
-	}()
-
-	return out
-}
 
 func StreamSeqEntries(ctx context.Context, cfg *config.Cache, backend repository.Backender, path []byte, num int) <-chan *model.Entry {
 	outCh := make(chan *model.Entry, runtime.GOMAXPROCS(0)*4)
@@ -149,59 +59,6 @@ func StreamSeqEntries(ctx context.Context, cfg *config.Cache, backend repository
 		}
 	}()
 	return outCh
-}
-
-func StreamRandomResponses(ctx context.Context, cfg *config.Cache, path []byte, num int) <-chan *model.Response {
-	outCh := make(chan *model.Response)
-
-	go func() {
-		defer close(outCh)
-
-		for req := range StreamRandomRequests(ctx, cfg, path, num) {
-			headers := http.Header{}
-			headers.Add("Content-Type", "application/json")
-			headers.Add("Vary", "Accept-Encoding, Accept-Language")
-
-			data := model.NewData(req.Rule(), http.StatusOK, headers, copiedBodyBytes(0))
-			resp, err := model.NewResponse(
-				data, req, cfg,
-				func(ctx context.Context) (*model.Data, error) {
-					// Dummy revalidator; always returns the same data.
-					return data, nil
-				},
-			)
-			if err != nil {
-				panic(err)
-			}
-			outCh <- resp
-		}
-	}()
-
-	return outCh
-}
-
-// GenerateRandomResponses generates a list of *model.Response, each linked to a random request and containing
-// random body data. Used for stress/load/benchmark testing of cache systems.
-func GenerateRandomResponses(cfg *config.Cache, path []byte, num int) []*model.Response {
-	list := make([]*model.Response, 0, num)
-	for _, req := range GenerateRandomRequests(cfg, path, num) {
-		headers := http.Header{}
-		headers.Add("Content-Type", "application/json")
-		headers.Add("Vary", "Accept-Encoding, Accept-Language")
-		data := model.NewData(req.Rule(), http.StatusOK, headers, copiedBodyBytes(0))
-		resp, err := model.NewResponse(
-			data, req, cfg,
-			func(ctx context.Context) (*model.Data, error) {
-				// Dummy revalidator; always returns the same data.
-				return data, nil
-			},
-		)
-		if err != nil {
-			panic(err)
-		}
-		list = append(list, resp)
-	}
-	return list
 }
 
 var responseBytes = []byte(`{
