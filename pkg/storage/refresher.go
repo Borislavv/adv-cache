6cf0ded7f2c756972505ba695f1e506a4ce5d087
Boris <glazunov2142@icloud.com>
2025-07-12 09:20:39 +0300

    feat: upgraded refresher.

--- pkg/storage/refresher.go
+++ pkg/storage/refresher.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"github.com/Borislavv/advanced-cache/pkg/rate"
 	"github.com/Borislavv/advanced-cache/pkg/storage/lru"
+	"runtime"
 	"strconv"
 	"time"
 
@@ -13,6 +14,8 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
+var numProducers = runtime.GOMAXPROCS(0)
+
 type Refresher interface {
 	Run()
 }
@@ -20,25 +23,35 @@ type Refresher interface {
 // Refresh is responsible for background refreshing of cache entries.
 // It periodically samples random shards and randomly selects "cold" entries
 // (from the end of each shard's Storage list) to refreshItem if necessary.
+// Communication: provider->consumer (MPSC).
 type Refresh struct {
 	ctx                 context.Context
 	cfg                 *config.Cache
 	balancer            lru.Balancer
-	scansRateLimiter    *rate.Limiter
-	requestRateLimiter  *rate.Limiter
+	storage             Storage
+	rateLogCh           chan int
 	refreshSuccessNumCh chan struct{}
 	refreshErroredNumCh chan struct{}
 	refreshItemsCh      chan *model.Entry
 }
 
 // NewRefresher constructs a Refresh.
-func NewRefresher(ctx context.Context, cfg *config.Cache, balancer lru.Balancer) *Refresh {
+func NewRefresher(ctx context.Context, cfg *config.Cache, balancer lru.Balancer, storage Storage) *Refresh {
+	scanRate := cfg.Cache.Refresh.Rate * 10
+	if scanRate < 1 {
+		scanRate = 1
+	}
+	scanBurst := cfg.Cache.Refresh.Rate / 10
+	if scanBurst < 1 {
+		scanBurst = 1
+	}
+
 	return &Refresh{
 		ctx:                 ctx,
 		cfg:                 cfg,
+		storage:             storage,
 		balancer:            balancer,
-		scansRateLimiter:    rate.NewLimiter(ctx, cfg.Cache.Refresh.ScanRate, cfg.Cache.Refresh.ScanRate/10),
-		requestRateLimiter:  rate.NewLimiter(ctx, cfg.Cache.Refresh.Rate, cfg.Cache.Refresh.Rate/10),
+		rateLogCh:           make(chan int, cfg.Cache.Refresh.Rate),
 		refreshSuccessNumCh: make(chan struct{}, cfg.Cache.Refresh.Rate),     // Successful refreshes counter channel
 		refreshErroredNumCh: make(chan struct{}, cfg.Cache.Refresh.Rate),     // Failed refreshes counter channel
 		refreshItemsCh:      make(chan *model.Entry, cfg.Cache.Refresh.Rate), // Failed refreshes counter channel
@@ -49,19 +62,51 @@ func NewRefresher(ctx context.Context, cfg *config.Cache, balancer lru.Balancer)
 // It runs a logger (if debugging is enabled), spawns a provider for sampling shards,
 // and continuously processes shard samples for candidate responses to refreshItem.
 func (r *Refresh) Run() {
-	r.runLogger()   // handle consumer stats and print logs
-	r.runConsumer() // scans rand items and checks whether they should be refreshed
-	r.runProducer() // produces items which should be refreshed on processing
+	r.runLogger()    // handle consumer stats and print logs
+	r.runConsumers() // scans rand items and checks whether they should be refreshed
+	r.runProducers() // produces items which should be refreshed on processing
 }
 
-func (r *Refresh) runProducer() {
+func (r *Refresh) runProducers() {
+	scanRate := r.cfg.Cache.Refresh.ScanRate / numProducers
+	if scanRate < 1 {
+		scanRate = 1
+	}
+	scanRateBurst := scanRate / 10 // burst is 10% of total rate
+	if scanRateBurst < 1 {
+		scanRateBurst = 1
+	}
+	for i := 0; i < numProducers; i++ {
+		r.runProducer(scanRate, scanRateBurst)
+	}
+}
+
+func (r *Refresh) runProducer(scanRate, scanBurst int) {
 	go func() {
+		ctx, cancel := context.WithTimeout(r.ctx, time.Second*10)
+		defer cancel()
+
+		scansRateLimiter := rate.NewLimiter(ctx, scanRate, scanBurst)
+
 		for {
 			select {
-			case <-r.ctx.Done():
+			case <-ctx.Done():
+				actualRate := int((float64(r.storage.Len()) / r.cfg.Cache.Refresh.TTL.Seconds()) / float64(numProducers))
+				if actualRate > r.cfg.Cache.Refresh.Rate {
+					actualRate = r.cfg.Cache.Refresh.Rate
+				} else if actualRate < 1 {
+					actualRate = 1
+				}
+
+				actualBurst := actualRate / 10
+				if actualBurst < 1 {
+					actualBurst = 1
+				}
+
+				r.runProducer(actualRate*10, actualBurst*10)
 				return
-			case <-r.scansRateLimiter.Chan():
-				if item := r.balancer.RandNode().RandItem(r.ctx); item.ShouldBeRefreshed(r.cfg) {
+			case <-scansRateLimiter.Chan():
+				if item := r.balancer.RandNode().RandItem(ctx); item.ShouldBeRefreshed(r.cfg) {
 					r.refreshItemsCh <- item
 				}
 			}
@@ -69,13 +114,46 @@ func (r *Refresh) runProducer() {
 	}()
 }
 
-func (r *Refresh) runConsumer() {
+func (r *Refresh) runConsumers() {
+	reqRate := r.cfg.Cache.Refresh.Rate
+	if reqRate < 1 {
+		reqRate = 1
+	}
+	reqRateBurst := reqRate / 10 // burst is 10% of total rate
+	if reqRateBurst < 1 {
+		reqRateBurst = 1
+	}
+	r.runConsumer(reqRate, reqRateBurst)
+}
+
+func (r *Refresh) runConsumer(reqRate, reqBurst int) {
 	go func() {
+		ctx, cancel := context.WithTimeout(r.ctx, time.Minute)
+		defer cancel()
+
+		requestsRateLimiter := rate.NewLimiter(ctx, reqRate, reqBurst)
+
 		for entry := range r.refreshItemsCh {
 			select {
-			case <-r.ctx.Done():
+			case <-ctx.Done():
+				actualRate := int(float64(r.storage.Len()) / r.cfg.Cache.Refresh.TTL.Seconds())
+				if actualRate > r.cfg.Cache.Refresh.Rate {
+					select {
+					case r.rateLogCh <- actualRate:
+					}
+					actualRate = r.cfg.Cache.Refresh.Rate
+				} else if actualRate < 1 {
+					actualRate = 1
+				}
+
+				actualBurst := actualRate / 10
+				if actualBurst < 1 {
+					actualBurst = 1
+				}
+
+				r.runConsumer(actualRate, actualBurst)
 				return
-			case <-r.requestRateLimiter.Chan():
+			case <-requestsRateLimiter.Chan():
 				go func() {
 					if err := entry.Revalidate(); err != nil {
 						r.refreshErroredNumCh <- struct{}{}
@@ -101,6 +179,12 @@ func (r *Refresh) runLogger() {
 			select {
 			case <-r.ctx.Done():
 				return
+			case actualRate := <-r.rateLogCh:
+				log.Warn().Msgf(
+					"[refresher] configured rate limit is %d per second "+
+						"but actually for have been able refresh all keys per TTL an appropriate value is %d per second",
+					r.cfg.Cache.Refresh.Rate, actualRate,
+				)
 			case <-r.refreshSuccessNumCh:
 				refreshesNumPer5Sec++
 			case <-r.refreshErroredNumCh:
