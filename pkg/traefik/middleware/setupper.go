15c78d6133234f078370949da5428310e5d4f3d1
Boris <glazunov2142@icloud.com>
2025-07-13 14:06:14 +0300

    Update setupper.go

--- pkg/traefik/middleware/setupper.go
+++ pkg/traefik/middleware/setupper.go
@@ -9,12 +9,12 @@ import (
 	sharded "github.com/Borislavv/advanced-cache/pkg/storage/map"
 )
 
-func (middleware *TraefikCacheMiddleware) setUpCache() {
-	shardedMap := sharded.NewMap[*model.Entry](middleware.ctx, middleware.cfg.Cache.Preallocate.PerShard)
-	middleware.backend = repository.NewBackend(middleware.cfg)
-	balancer := lru.NewBalancer(middleware.ctx, shardedMap)
-	middleware.store = lru.NewStorage(middleware.ctx, middleware.cfg, balancer, middleware.backend, lfu.NewTinyLFU(middleware.ctx), shardedMap)
-	middleware.refresher = storage.NewRefresher(middleware.ctx, middleware.cfg, balancer, middleware.store)
-	middleware.dumper = storage.NewDumper(middleware.cfg, shardedMap, middleware.store, middleware.backend)
-	middleware.evictor = storage.NewEvictor(middleware.ctx, middleware.cfg, middleware.store, balancer)
+func (m *TraefikCacheMiddleware) setUpCache() {
+	shardedMap := sharded.NewMap[*model.Entry](m.ctx, m.cfg.Cache.Preallocate.PerShard)
+	m.backend = repository.NewBackend(m.cfg)
+	balancer := lru.NewBalancer(m.ctx, shardedMap)
+	m.store = lru.NewStorage(m.ctx, m.cfg, balancer, m.backend, lfu.NewTinyLFU(m.ctx), shardedMap)
+	m.refresher = storage.NewRefresher(m.ctx, m.cfg, balancer, m.store)
+	m.dumper = storage.NewDumper(m.cfg, shardedMap, m.store, m.backend)
+	m.evictor = storage.NewEvictor(m.ctx, m.cfg, m.store, balancer)
 }
