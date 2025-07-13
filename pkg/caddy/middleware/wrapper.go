b5d7fb7357f21f4dbda156605591158ab6a75316
Boris <glazunov2142@icloud.com>
2025-07-13 09:22:21 +0300

    Update wrapper.go

--- pkg/caddy/middleware/wrapper.go
+++ pkg/caddy/middleware/wrapper.go
@@ -3,19 +3,31 @@ package advancedcache
 import (
 	"bytes"
 	"net/http"
+	"sync"
 )
+
+var capturersPool = sync.Pool{
+	New: func() interface{} {
+		return &captureRW{
+			header:      make(http.Header),
+			body:        new(bytes.Buffer),
+			status:      0,
+			wroteHeader: false,
+		}
+	},
+}
 
 type captureRW struct {
 	header      http.Header
-	body        bytes.Buffer
+	body        *bytes.Buffer
 	status      int
 	wroteHeader bool
 }
 
-func newCaptureRW() *captureRW {
-	return &captureRW{
-		header: make(http.Header),
-		status: http.StatusOK,
+func newCaptureRW() (capturer *captureRW, releaseFn func()) {
+	capturer = capturersPool.Get().(*captureRW)
+	return capturer, func() {
+		capturersPool.Put(capturer.reset())
 	}
 }
 
@@ -35,4 +47,12 @@ func (c *captureRW) Write(b []byte) (int, error) {
 		c.WriteHeader(http.StatusOK)
 	}
 	return c.body.Write(b)
+}
+
+func (c *captureRW) reset() *captureRW {
+	c.body.Reset()
+	for key, _ := range c.header {
+		delete(c.header, key)
+	}
+	return c
 }
