b7e2ab7dc075325a46f60ac4f2856c8085d05203
Boris <glazunov2142@icloud.com>
2025-07-13 14:04:43 +0300

    feat: http header helper.

---
+++ pkg/header/last_modified.go
@@ -0,0 +1,20 @@
+package header
+
+import (
+	"github.com/Borislavv/advanced-cache/pkg/model"
+	"net/http"
+	"time"
+	"unsafe"
+)
+
+var lastModifiedKey = "Last-Modified"
+
+func SetLastModified(w http.ResponseWriter, entry *model.Entry, status int) {
+	if status == http.StatusOK {
+		bts := time.Unix(0, entry.WillUpdateAt()-entry.Rule().TTL.Nanoseconds()).AppendFormat(nil, http.TimeFormat)
+		w.Header().Set(lastModifiedKey, unsafe.String(unsafe.SliceData(bts), len(bts)))
+	} else {
+		bts := time.Unix(0, entry.WillUpdateAt()-entry.Rule().ErrorTTL.Nanoseconds()).AppendFormat(nil, http.TimeFormat)
+		w.Header().Set(lastModifiedKey, unsafe.String(unsafe.SliceData(bts), len(bts)))
+	}
+}
