1043b56a454ca7323355e1a40c42b11ec8d923d4
Boris <glazunov2142@icloud.com>
2025-07-27 17:55:08 +0300

    Update pointer.go

--- pkg/model/pointer.go
+++ pkg/model/pointer.go
@@ -1,1 +1,26 @@
 package model
+
+import (
+	"github.com/Borislavv/advanced-cache/pkg/config"
+)
+
+type VersionPointer struct {
+	version uint32
+	*Entry
+}
+
+func NewVersionPointer(entry *Entry) *VersionPointer {
+	return &VersionPointer{version: entry.Version(), Entry: entry}
+}
+
+func (v *VersionPointer) Acquire() bool {
+	return v != nil && v.Entry.Acquire(v.version)
+}
+
+func (v *VersionPointer) Version() uint32 {
+	return v.version
+}
+
+func (v *VersionPointer) ShouldBeRefreshed(cfg *config.Cache) bool {
+	return v != nil && v.Entry.ShouldBeRefreshed(cfg)
+}
