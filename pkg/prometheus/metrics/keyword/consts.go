d11397f8aedd931df02678ec08dbd936517cde87
Boris <glazunov2142@icloud.com>
2025-08-15 16:11:34 +0300

    Update consts.go

--- pkg/prometheus/metrics/keyword/consts.go
+++ pkg/prometheus/metrics/keyword/consts.go
@@ -1,6 +1,7 @@
 package keyword
 
 var (
+	AvgDuration              = "adv_cache_avg_duration_ns"
 	RPS                      = "adv_cache_rps"
 	Total                    = "adv_cache_total"   // num of total requests
 	Errored                  = "adv_cache_errors"  // num of errors
